0d6f1a2d9de73ed45e98883ae8b7ecad5ee7c41c
Victor Engmark <victor.engmark@gmail.com>
2012-05-03 19:42:48 +0200

    Documentation formatting

--- unrarr.sh
+++ unrarr.sh
@@ -1,42 +1,45 @@
 #!/bin/sh
 #
 # NAME
-#    unrarr.sh - UnRAR recursive
+#        unrarr.sh - UnRAR recursive
 #
 # SYNOPSIS
-#    unrarr.sh [options] directory...
+#        unrarr.sh [options] directory...
 #
 # OPTIONS
-#    -d,--delete    Delete files after successfully uncompressing them
-#    -v             Verbose output
+#        -d, --delete
+#               Delete files after successfully uncompressing them
+#
+#        -v
+#               Verbose output
 #
 # EXAMPLE
-#    ./unrarr.sh ~/downloads
+#        ./unrarr.sh ~/downloads
 #
-#    Uncompress all RAR files in ~/downloads
+#        Uncompress all RAR files in ~/downloads
 #
 # DESCRIPTION
-#    Unrar (and optionally delete) recursively in directories.
+#        Unrar (and optionally delete) recursively in directories.
 #
 # BUGS
-#    Email bugs to victor dot engmark at gmail dot com. Please include the
-#    output of running this script in verbose mode (-v).
+#        Email bugs to victor dot engmark at gmail dot com. Please include the
+#        output of running this script in verbose mode (-v).
 #
 # COPYRIGHT AND LICENSE
-#    Copyright (C) 2009 Victor Engmark
+#        Copyright (C) 2009, 2012 Victor Engmark
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#        GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
 
